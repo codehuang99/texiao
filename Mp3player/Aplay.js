f76999fc3364bf88568724d74f996bae8b743af9
codehuang99 <you@example.com>
2020-04-07 21:11:09 +0800

    update

---
+++ Mp3player/Aplay.js
@@ -0,0 +1,14 @@
+<!-- require APlayer -->
+<link rel="stylesheet" href="./APlayer.min.css">
+<script src="./APlayer.min.js"></script>
+<!-- require MetingJS -->
+<script src="./Meting.min.js"></script>
+
+<meting-js
+	server="netease"
+	type="playlist"
+	id="2536451796"
+	autoplay="true"
+	fixed="true"
+	>
+</meting-js>
